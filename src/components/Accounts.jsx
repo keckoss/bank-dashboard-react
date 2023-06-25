1486adfa1150137971c12691a30ef45f1d199ce9
keckoss13 <73256718+keckoss13@users.noreply.github.com>
2023-06-25 03:15:50 +0200

    add see more button anc font for money

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -9,15 +9,18 @@ function Accounts(props) {
           >
             <div className={`nomcompte ${index % 2 === 0 ? "even" : "odd"}`}>
               <h2 className="enfantcompte">{element.name}</h2>
-              <p className="enfantcompte">{element.balance} €</p>
+              <p className="enfantcompte policemontant">{element.balance} €</p>
             </div>
             {element.operations.map((element) => (
               <div className="transactions">
                 <p className="ladate">{element.date}</p>
                 <p className="transacdesc">{element.description}</p>
-                <p>{element.amount} €</p>
+                <p className="policemontant">{element.amount} €</p>
               </div>
             ))}
+            <div className="but">
+              <button className="button">SEE MORE</button>
+            </div>
           </div>
         );
       })}
